6abc68f95ef69550e566959a7a62f1b4a2952207
Priyapta <priaptanaufal@gmail.com>
2024-09-17 12:01:31 +0700

    update add project

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
     itemName = models.CharField(max_length=255)
     itemDescription = models.TextField()
     itemPrice = models.DecimalField(max_digits=10, decimal_places=2)
-        
+    itemid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
--- palgop_gamepass/settings.py
+++ palgop_gamepass/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x)1qy$vb8gpq0t@$6z*2m!0t_&a216)dhf*6dbl+5_&so1#tml
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","priyapta-naufal-palgopgamepass.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","priyapta-naufal-palgopgamepass.pbp.cs.ui.ac.id","http://priyapta-naufal-palgopgamepass.pbp.cs.ui.ac.id/","https://priyapta-naufal-palgopgamepass.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'palgop_gamepass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
