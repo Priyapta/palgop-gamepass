74e837c738741283f877b00db95204ebcc9647e8
Priyapta <priaptanaufal@gmail.com>
2024-09-24 18:57:46 +0700

    add login

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     itemName = models.CharField(max_length=255)
     itemDescription = models.TextField()
     itemPrice = models.DecimalField(max_digits=10, decimal_places=2)
--- palgop_gamepass/settings.py
+++ palgop_gamepass/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x)1qy$vb8gpq0t@$6z*2m!0t_&a216)dhf*6dbl+5_&so1#tml'
 
 # SECURITY WARNING: don't run with debug turned on in production! 
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","priyapta-naufal-palgopgamepass.pbp.cs.ui.ac.id"]
 
