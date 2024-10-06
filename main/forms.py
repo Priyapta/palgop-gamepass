cff2f01f168b9cf9289abe91dc8dc4cbfb762c7a
Priyapta <priaptanaufal@gmail.com>
2024-10-06 12:39:43 +0700

    update tugas 05

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.utils.html import strip_tags
 
 class ProductEntryForm(ModelForm):
     class Meta:
--- main/views.py
+++ main/views.py
@@ -12,16 +12,17 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
+    
     context = {
         'name' : request.user.username,
         'npm': '2306245106',
         'class': 'PBP D',
-        'product_entries' : product_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -41,11 +42,11 @@ def create_product(request):
     return render(request, "create_product_entry.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
     
 def show_xml_by_id(request, id):
@@ -78,9 +79,12 @@ def login_user(request):
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
+     
 
    else:
-      form = AuthenticationForm(request)
+        messages.error(request, "Invalid username or password. Please try again.")
+        form = AuthenticationForm(request)
+      
    context = {'form': form}
    return render(request, 'login.html', context)
 
@@ -113,3 +117,23 @@ def delete_product(request, id):
     product.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    item_name = strip_tags(request.POST.get("item_name"))
+    item_price = strip_tags(request.POST.get("item_price"))
+    item_description = strip_tags(request.POST.get("item_description"))
+    print(item_name,item_price,item_description)
+    user = request.user
+
+    new_product = Product(
+        item_name=item_name,
+        item_price=item_price,
+        item_description=item_description,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
