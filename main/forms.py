8f67ca1f77598b8a2b394d2d7aca7e4297f763d0
Priyapta <priaptanaufal@gmail.com>
2024-10-09 07:26:53 +0700

    clean update

--- main/forms.py
+++ main/forms.py
@@ -6,3 +6,15 @@ class ProductEntryForm(ModelForm):
     class Meta:
         model = Product
         fields = ["item_name", "item_price", "item_description"]
+        
+    def clean_itemName(self):
+        item_name = self.cleaned_data["item_name"]
+        return strip_tags(item_name)
+    
+    def clean_itemDescription(self):
+        item_description = self.cleaned_data["item_description"]
+        return strip_tags(item_description)
+
+    def clean_item_price(self):
+        item_price = self.cleaned_data["item_price"]
+        return strip_tags(item_price)
